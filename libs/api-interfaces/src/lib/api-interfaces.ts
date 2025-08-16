c14e46a050fc1211efb37077037ef24783240771
jurilu <lubonjajurgen1@gmail.com>
2025-08-16 15:31:38 +0200

    1. Alwasy set the data model first.

--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,3 +1,14 @@
 export interface Message {
   message: string;
 }
+
+export interface BaseEntity {
+  id: string | null;
+}
+
+export interface Widget extends BaseEntity {
+  title: string;
+  description: string;
+  
+}
+
