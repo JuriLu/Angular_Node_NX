bded19e01c84bcfb829b2d46e8376187c475f571
jurilu <lubonjajurgen1@gmail.com>
2025-09-20 11:06:23 +0200

    feat(widget-list): Managing widgets list component. [FEM-2]

--- apps/dashboard/src/app/widgets/widgets-list/widgets-list.component.ts
+++ apps/dashboard/src/app/widgets/widgets-list/widgets-list.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit } from '@angular/core';
+import { Widget } from '@fem/api-interfaces';
 
 @Component({
   selector: 'fem-production-angular-widgets-list',
   templateUrl: './widgets-list.component.html',
-  styleUrls: ['./widgets-list.component.scss']
+  styleUrls: ['./widgets-list.component.scss'],
 })
-export class WidgetsListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class WidgetsListComponent {
+  @Input() widgets: Widget[];
+  @Input() readonly: boolean = false;
+  @Input() selected: EventEmitter<boolean> = new EventEmitter();
+  @Input() deleted: EventEmitter<boolean> = new EventEmitter();
 }
